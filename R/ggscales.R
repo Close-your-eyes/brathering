02ea91d1e434ab2240c8e61987a23697638023da
Close-your-eyes <vonskopnik@pm.me>
2025-05-22 21:43:30 +0200

    up

---
+++ R/ggscales.R
@@ -0,0 +1,125 @@
+#' Spectral color scale
+#'
+#' Shortcut function to a spectral color scale.
+#'
+#' @param colors see ggplot2::scale_color_gradientn
+#' @param values see ggplot2::scale_color_gradientn
+#' @param name see ggplot2::scale_color_gradientn
+#' @param na.value see ggplot2::scale_color_gradientn
+#' @param guide see ggplot2::scale_color_gradientn
+#' @param ... see ggplot2::scale_color_gradientn
+#'
+#' @return ggplot2::scale_color_gradientn
+#' @export
+#'
+#' @examples
+scale_color_spectral <- function(colors = colrr::col_pal("RColorBrewer::Spectral", direction = -1),
+                                 values = NULL,
+                                 name = waiver(),
+                                 na.value = "grey50",
+                                 guide = "colorbar",
+                                 ...) {
+    if (!requireNamespace("colrr", quietly = T)) {
+        devtools::install_github("https://github.com/Close-your-eyes/colrr")
+    }
+    if (is.null(values)) {
+        values <- seq(0, 1, length.out = length(colors))
+    }
+    ggplot2::scale_color_gradientn(
+        colours = colors,
+        values = scales::rescale(values),
+        name = name,
+        na.value = na.value,
+        guide = guide,
+        ...
+    )
+}
+
+#' Spectral fill scale
+#'
+#' Shortcut function to a spectral fill scale.
+#'
+#' @param colors see ggplot2::scale_fill_gradientn
+#' @param values see ggplot2::scale_fill_gradientn
+#' @param name see ggplot2::scale_fill_gradientn
+#' @param na.value see ggplot2::scale_fill_gradientn
+#' @param guide see ggplot2::scale_fill_gradientn
+#' @param ... see ggplot2::scale_fill_gradientn
+#'
+#' @return ggplot2::scale_fill_gradientn
+#' @export
+#'
+#' @examples
+scale_fill_spectral <- function(colors = colrr::col_pal("RColorBrewer::Spectral", direction = -1),
+                                values = NULL,
+                                name = waiver(),
+                                na.value = "grey50",
+                                guide = "colorbar",
+                                ...) {
+    if (!requireNamespace("colrr", quietly = T)) {
+        devtools::install_github("https://github.com/Close-your-eyes/colrr")
+    }
+    if (is.null(values)) {
+        values <- seq(0, 1, length.out = length(colors))
+    }
+    ggplot2::scale_fill_gradientn(
+        colours = colors,
+        values = scales::rescale(values),
+        name = name,
+        na.value = na.value,
+        guide = guide,
+        ...
+    )
+}
+
+#' Custom color scale
+#'
+#' @param colors see ggplot2::scale_color_manual
+#' @param name see ggplot2::scale_color_manual
+#' @param na.value see ggplot2::scale_color_manual
+#' @param ... see ggplot2::scale_color_manual
+#'
+#' @return ggplot2::scale_color_manual
+#' @export
+#'
+#' @examples
+scale_color_custom <- function(colors = colrr::col_pal("custom"),
+                               name = waiver(),
+                               na.value = "grey50",
+                               ...) {
+    if (!requireNamespace("colrr", quietly = T)) {
+        devtools::install_github("https://github.com/Close-your-eyes/colrr")
+    }
+    ggplot2::scale_color_manual(
+        values = colors,
+        name = name,
+        na.value = na.value,
+        ...
+    )
+}
+
+#' Custom fill scale
+#'
+#' @param colors see ggplot2::scale_fill_manual
+#' @param name see ggplot2::scale_fill_manual
+#' @param na.value see ggplot2::scale_fill_manual
+#' @param ... see ggplot2::scale_fill_manual
+#'
+#' @return ggplot2::scale_fill_manual
+#' @export
+#'
+#' @examples
+scale_fill_custom <- function(colors = colrr::col_pal("custom"),
+                              name = waiver(),
+                              na.value = "grey50",
+                              ...) {
+    if (!requireNamespace("colrr", quietly = T)) {
+        devtools::install_github("https://github.com/Close-your-eyes/colrr")
+    }
+    ggplot2::scale_fill_manual(
+        values = colors,
+        name = name,
+        na.value = na.value,
+        ...
+    )
+}

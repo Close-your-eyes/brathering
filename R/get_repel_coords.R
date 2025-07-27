c225a0fe2716c21554db1af86358df4b79c63daa
Close-your-eyes <vonskopnik@pm.me>
2025-07-27 17:40:20 +0200

    repel_coords up

--- R/get_repel_coords.R
+++ R/get_repel_coords.R
@@ -5,11 +5,13 @@
 #' pre-computed or so. Hence, it is not straight forward to extract their
 #' repelled positions.
 #'
-#' @param obj ggplot object or data frame with first columns being xy-coords
+#' @param obj data frame with first columns being xy-coords and third column
+#' labels
 #' @param width passed to grid::viewport, may be subject to iteration to obtain
 #' optimal repelled coordinates
 #' @param height passed to grid::viewport
 #' @param ... arguments to ggrepel::geom_text_repel
+#' @param max.overlaps ggrepel::geom_text_repel argument
 #'
 #' @return data frame
 #' @export
@@ -17,50 +19,54 @@
 #' @examples
 #' library(ggplot2)
 #' df <- data.frame(x=rnorm(10), y=rnorm(10), z = letters[1:10])
-#' gg <- ggplot(df, aes(x,y)) +
-#'     geom_point()
 #' gg <-
-#'     gg +
-#'     ggrepel::geom_label_repel(aes(label = z)) +
-#'     geom_text(data = dplyr::left_join(df, get_repel_coords(gg)),
-#'               aes(x = x_repel,y = y_repel, label = z), color = "hotpink")
+#'     ggplot(df, aes(x = x, y = y, label = z)) +
+#'     geom_point() +
+#'     ggrepel::geom_label_repel() +
+#'     geom_text(data = get_repel_coords(obj = df), color = "hotpink")
 #' gg
-get_repel_coords <- function(obj, width = 2, height = 2, ...) {
+get_repel_coords <- function(obj,
+                             width = 1,
+                             height = 1,
+                             max.overlaps = Inf,
+                             ...) {
     grid::grid.newpage()
     grid::pushViewport(grid::viewport(width = width, height = height))
 
-    if (is.data.frame(obj)) {
-        obj <- ggplot2::ggplot(obj,
-                                 ggplot2::aes(
-                                     !!rlang::sym(names(obj)[1]),
-                                     !!rlang::sym(names(obj)[2]))) +
-            ggplot2::geom_point()
-    }
-
-    xvar <- rlang::as_name(rlang::quo_get_expr(obj[["mapping"]][["x"]]))
-    yvar <- rlang::as_name(rlang::quo_get_expr(obj[["mapping"]][["y"]]))
-
-    g <- obj +
+    g <- ggplot2::ggplot(obj,
+                         ggplot2::aes(
+                             x = !!rlang::sym(names(obj)[1]),
+                             y = !!rlang::sym(names(obj)[2]))) +
         ggrepel::geom_text_repel(
-            mapping = ggplot2::aes(!!rlang::sym(xvar), !!rlang::sym(yvar)),
-            label = ".",
-            data = obj[["data"]],
-            max.overlaps = Inf,
+            mapping = ggplot2::aes(label = !!rlang::sym(names(obj)[3])),
+            max.overlaps = max.overlaps,
             ...)
 
-    labelvar <- obj[["labels"]][["label"]]
-    plotlims <- brathering::gg_lims(obj)
+    plotlims <- brathering::gg_lims(g)
     tree <- grid::getGrob(gTree = grid::grid.force(x = ggplot2::ggplotGrob(g), draw = F), gPath = "textrepeltree", grep = T)
     children <- grep(pattern = "textrepelgrob", x = grid::childNames(tree), value = T)
+    names(children) <- obj[,3]
 
-    get_xy <- function(n, tree, plotlims) {
-        grob <- grid::getGrob(tree, n)
-        data.frame(
-            x_repel = plotlims[["x"]][1] + diff(plotlims[["x"]]) * grid::convertX(grob$x, "native", valueOnly = T),
-            y_repel = plotlims[["y"]][1] + diff(plotlims[["y"]]) * grid::convertY(grob$y, "native", valueOnly = T))
-    }
-
-    return(cbind(obj[["data"]][,c(xvar, yvar, labelvar)],
-                 purrr::map_dfr(children, get_xy, tree = tree, plotlims = plotlims)))
+    return(purrr::map_dfr(
+                   children,
+                   get_xy,
+                   tree = tree,
+                   plotlims = plotlims,
+                   xvar = names(obj)[1],
+                   yvar = names(obj)[2],
+                   .id = names(obj)[3]
+                 ))
 }
 
+get_xy <- function(n,
+                   tree,
+                   plotlims,
+                   xvar,
+                   yvar) {
+    grob <- grid::getGrob(tree, n)
+    df <- data.frame(
+        x = plotlims[[1]][1] + diff(plotlims[[1]]) * grid::convertX(grob$x, "native", valueOnly = T),
+        y = plotlims[[2]][1] + diff(plotlims[[2]]) * grid::convertY(grob$y, "native", valueOnly = T))
+    names(df) <- c(xvar, yvar)
+    return(df)
+}
